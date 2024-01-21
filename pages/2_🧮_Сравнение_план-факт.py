dbcf4ea2fa9bb130f2a2c32026981d5d63b5e68c
EarlXD <86591772+EarlXD@users.noreply.github.com>
2024-01-21 22:48:35 +0500

    Update 2_🧮_Сравнение_план-факт.py

--- pages/2_🧮_Сравнение_план-факт.py
+++ pages/2_🧮_Сравнение_план-факт.py
@@ -8,7 +8,7 @@ from plotly.subplots import make_subplots
 
 st.set_page_config(layout="wide")
 
-df_mp, df_api = read_and_prepare_df('E:/work/strlit/')
+df_mp, df_api = read_and_prepare_df('')
 
 with st.sidebar:
     managers = st.multiselect('Выбор специалиста',
