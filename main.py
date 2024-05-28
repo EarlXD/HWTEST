725b7e51f83c0d4714199bc2c2c078041f43ca94
EarlXD <86591772+EarlXD@users.noreply.github.com>
2024-05-28 21:28:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -15,23 +15,6 @@ st.set_page_config(
         page_icon="📈", layout='wide'
         )
 
-with open('config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
 
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized'])
-
-if st.session_state["authentication_status"]:
-    authenticator.logout(location='sidebar')
-    st.title(f'Welcome, {st.session_state["name"]}')
-elif st.session_state["authentication_status"] is False:
-    authenticator.login(location='sidebar')
-    st.error('Username/password is incorrect')
-elif st.session_state["authentication_status"] is None:
-    authenticator.login(location='sidebar')
     
     
