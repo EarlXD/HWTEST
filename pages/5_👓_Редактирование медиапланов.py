d99442a741a9b5c551d62ded6f95cb3f82825dfc
EarlXD <86591772+EarlXD@users.noreply.github.com>
2024-05-28 21:33:56 +0500

    Update 5_👓_Редактирование медиапланов.py

--- pages/5_👓_Редактирование медиапланов.py
+++ pages/5_👓_Редактирование медиапланов.py
@@ -6,9 +6,6 @@ from path_lib import *
 import datetime
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-import streamlit_authenticator as stauth
-import yaml
-from yaml.loader import SafeLoader
 import ad_functions_nova
 from itertools import chain
 
@@ -19,31 +16,7 @@ def save_new_mp(df_mp):
     df_mp = df_mp.drop_duplicates(subset=['campaign_id_uniq', 'start_date', 'end_date'], keep='last')
     df_mp.to_pickle(path + 'data/df_mp.pkl', index=False, encoding='utf-8-sig')
 
-with open('config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
-
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized'])
-
-if st.session_state["authentication_status"]:
-    pass
-elif st.session_state["authentication_status"] is False:
-    authenticator.login(location='sidebar')
-    st.error('Username/password is incorrect')
-if st.session_state['authentication_status'] == None or st.session_state['authentication_status'] == False:
-    authenticator.login(location='sidebar')
-    
-
-if 'authentication_status' not in st.session_state:
-    st.stop()
-if st.session_state['authentication_status'] == None:
-    st.stop()
-
-df_mp = mp_df_read(path)
+df_mp = mp_df_read('')
 df_mp = clean_column_values(df_mp, 'campaign_id_uniq')
 
 with st.sidebar:
